92103173e451d8e8e8f2464bd9a4c31dfa36da1d
alexisdlr <alexisdlr3009@hotmail.com>
2024-01-01 21:16:16 -0600

    quizz

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -13,7 +13,7 @@ interface State {
   reset: () => void
 }
 
-const API_URL = import.meta.env.PROD ? 'https://midu-react-13.surge.sh/' : 'http://localhost:5173/'
+const API_URL = import.meta.env.PROD ? 'https://https://javascript-quizz-gilt.vercel.app/' : 'http://localhost:5173/'
 
 export const useQuestionStore = create<State>()(devtools(persist((set, get) => {
   return {
